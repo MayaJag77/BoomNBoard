6c6641afe73aced18a3a252c6b6f80b86a2e345e
“Maya <maya.h.jagielka@bbc.co.uk>
2026-03-11 15:25:43 +0000

    Creating model for database

--- BoomNBoard/app/models.py
+++ BoomNBoard/app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    userID = models.CharField(primary_key=True, max_length=128, unique=True)
+    username = models.CharField(max_length=128, unique=True)
+    password = models.CharField(max_length=128)
+    email = models.EmailField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.username
+
+class Sounds(models.Model):
+    soundID = models.CharField(primary_key=True, max_length=5, unique=True)
+    uploadedBy = models.ForeignKey(User, on_delete=models.CASCADE)
+    soundFile = models.URLField()
+    name = models.CharField(max_length=40)
+    category = models.CharField(default=0, max_length=15)
+    description = models.CharField(max_length=500, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class SavedSounds(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    sound = models.ForeignKey(Sounds, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'sound')
+
+    def __str__(self):
+        return f"{self.user.username} saved {self.sound.name}"
