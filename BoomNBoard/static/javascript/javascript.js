93453a0c8adc3469ac4a9e6cd2edc472c622f8a1
Oliver Forbes <3101438f@student.gla.ac.uk>
2026-03-19 14:11:41 +0000

    Added play sound functionality for button images

--- BoomNBoard/static/javascript/javascript.js
+++ BoomNBoard/static/javascript/javascript.js
@@ -57,4 +57,12 @@ function addToFavourite(clickedImage){
     
 }
 
-
+function playSound(audioId) {
+    const audio = document.getElementById(audioId);
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+}
